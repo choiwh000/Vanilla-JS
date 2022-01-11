777193c2f774a60216a8a3be02e7b2bb4f330090
choiwh000 <cwh915@naver.com>
2022-01-11 16:27:20 +0900

    update

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const Hidden_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+    function onLoginSubmit(event) {
+        event.preventDefault();
+        loginForm.classList.add(Hidden_CLASSNAME);
+        const username = loginInput.value;
+        localStorage.setItem( USERNAME_KEY , username);
+        paintGreeting(username);
+    }
+
+    function paintGreeting(username) {
+        greeting.classList.remove(Hidden_CLASSNAME);
+        greeting.innerText = `Hello ${username}`;
+    }
+
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+ 
+    if(savedUsername === null){
+        loginForm.classList.remove(Hidden_CLASSNAME);
+        loginForm.addEventListener('submit', onLoginSubmit)
+    } else {
+        paintGreeting(savedUsername);
+    }
