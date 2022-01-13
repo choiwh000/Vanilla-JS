1806223c3e4f5af93023c47bb8e75e9a36d384af
choiwh000 <cwh915@naver.com>
2022-01-13 14:51:54 +0900

    weather Update

---
+++ js/weather.js
@@ -0,0 +1,18 @@
+const API_KEY = "59229def0c12d575d02afbf50b4d4735"
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoeror() {
+    alert("Geoeoror");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoeror);
